a7d3e6430bbc9a424ece24f56642ad0b8cc85473
lina325 <2402749@spsd.sk.ca>
2024-11-28 11:02:56 -0600

    Troubleshooting current errors
    Figure out how song map will work

--- sketch.js
+++ sketch.js
@@ -9,52 +9,64 @@ const NUM_OF_COLS = 4;
 // const NUM_OF_ROWS = 20;
 let numOfRows;
 let grid;
+let cellSize = 127;
 
-let screenState = "selection";
+let screenState = "play";
 let selection;
-let songList = loadStrings("songs.txt"); 
+let songList; 
 let level;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  numOfRows = height/127;
+  numOfRows = height/cellSize;
   
   grid = new Grid(NUM_OF_COLS, numOfRows);
   grid = grid.generate();
+
+  songList = loadStrings("songs.txt");
 }
 
 function draw() {
   if (screenState === "start") {
-
+    background(50);
+    displayStartScreen();
   }
   else if (screenState === "selection") {
+    background(100);
     displaySelectionScreen();
   }
   else if (screenState === "play") {
-    background(220);
-    // grid.display(grid);
+    background(200);
+    // grid.display();
     display(grid);
 
-    selectSong();
+    // loadSong();
   }
   else if (screenState === "score") {
+    background(255);
     displayScore();
   }
 }
 
+function displayStartScreen() {
+
+}
+
 function displaySelectionScreen() {
   // Set images and text overtop
-  text();
+  text("Placeholder", width/4, height/2);
 
   if (mouseX > 0 && mouseX < width/2 && mouseY > 0 && mouseY < height && mouseIsPressed) {
     selection = 0;
+    screenState = "play";
   }
   else if (mouseX > width/2 && mouseX < width && mouseY > 0 && mouseY < height && mouseIsPressed) {
     selection = 1;
+    screenState = "play";
   }
 }
 
-function selectSong() {
+function loadSong() {
   if (selection === 1) {
     // Load song and map
     level = loadStrings(`${songList[choice]}.txt`);
@@ -87,10 +99,14 @@ function display(array) { //Temporary until you figure out what's wrong (rip)
   for (let x = 0; x < 4; x++) {
     for (let y = 0; y < 20; y ++) {
       if (array[x][y] === 0) {
-        rect(x * 127, y * 127, 127, 127);
+        noStroke();
+        rect(x * cellSize + (width/2 - cellSize*2), y * cellSize, cellSize, cellSize);
+
+        line(x * cellSize + (width/2 - cellSize*2), 0, x * cellSize + (width/2 - cellSize*2), height);
       }
     }
   }
+  line(4 * cellSize + (width/2 - cellSize*2), 0, 4 * cellSize + (width/2 - cellSize*2), height);
 }
 
 class Grid {
@@ -113,21 +129,23 @@ class Grid {
     return newGrid;
   }
 
-  display(array) {
+  display() {
     for (let x = 0; x < this.cols; x++) {
       for (let y = 0; y < this.rows; y ++) {
-        if (array[x][y] === 0) {
-          rect(x * this.cellWidth, y * this.cellHeight, this.cellWidth, this.cellHeight);
-        }
+        // if (array[x][y] === 0) {
+        rect(x * this.cellWidth, y * this.cellHeight, this.cellWidth, this.cellHeight);
+        // }
       }
     }
   }
 }
 
 class TapTile {
-  constructor(){
-    this.x;
-    this.y;
+  constructor(x) {
+    this.tileWidth = cellSize;
+    this.tileHeight = 42;
+    this.x = x;
+    this.y = 0 - this.tileHeight;
   }
 
   display() {
@@ -137,4 +155,14 @@ class TapTile {
   checkHit() {
     // Use dist ?
   }
+}
+
+class HoldTile {
+  constructor() {
+    this.x;
+    this.y;
+
+  }
+
+  // Can use keyIsPressed for this? 
 }
